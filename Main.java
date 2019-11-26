881aaaac15edae7e4759989218bf973e8e352c14
birdshe <qaz0975417436@gmail.com>
2019-11-27 07:48:09 +0800

    add record.java, modify Main.java

--- Main.java
+++ Main.java
@@ -5,20 +5,23 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print(question.WhatDo);
-		System.out.print("(1=食物 , 2=衣服 , 3=娛樂)");
-		Scanner a=new Scanner(System.in);
-		int choose1=0;
-		choose1=a.nextInt();
-		if(choose1==1) {
-			
+		Scanner s=new Scanner(System.in);
+		int choose1=0; //記錄第一個選擇
+		record user=new record(); //用來計錄使用者選項的類別
+		
+		System.out.println(question.WhatDo); //第一個問題
+		System.out.println(question.WhatDoSelect); //顯示選項
+		choose1=s.nextInt();
+		
+		while(user.setWhatDo(choose1)==false){ //檢視使用者輸入的數字
+			System.out.println("輸入有誤，請重新輸入");
+			System.out.println(question.WhatDoSelect);
+			choose1=s.nextInt();
 		}
-		if(choose1==2) {
-			
-		}
-		if(choose1==3) {
-			//hello
-		}
+		
+		user.show();
+		
+		
 	}
 
 }
--- question.java
+++ question.java
@@ -1,7 +1,10 @@
 package What_to_do;
 
-public class question {
+class question {
 	public final static String WhatDo="你想做什麼 ?"; 
 	public final static String WhatFood="你想吃什麼?";
 	public final static String WhatPlay="你想玩什麼?";
+	public final static String WhatDoSelect="1=食物\n2=衣服\n3=娛樂";
+	
+	
 }
