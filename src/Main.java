a427e7783413a10e3243b4af2092d362e4fb44b3
birdshe <qaz0975417436@gmail.com>
2019-12-15 23:16:51 +0800

    just modify Main

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,19 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-class frame extends JFrame{
-	//視窗物件
-	private JPanel p;
-	private JButton btnMove;
-	
-	public frame(String name) {
-		//視窗的建構
-		super(name);
-		this.setSize(300, 300);
+
+public class Main extends JFrame{
+	public Main() {
+		super("今天要做什麼");
+		this.setSize(800, 600);
 		//設定視窗的長跟寛
 		this.setLocation(200, 100);
 		//視窗的出現位置 x y
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
-}
-
-public class Main extends JFrame{
 	public static void main(String[] args) {
-		frame myf=new frame("hello");
+		new Main();
 	}
 		
 }
